33171b47f105a0f7835f5eeb88fd7022bee9c1e1
Anders Westrup <anders@westrup.se>
2018-10-09 21:41:08 +0200

    Cleanup

--- src/nRF5_SDK/examples/ble_peripheral/ble_app_beacon/main.c
+++ src/nRF5_SDK/examples/ble_peripheral/ble_app_beacon/main.c
@@ -1,52 +1,3 @@
-/**
- * Copyright (c) 2014 - 2018, Nordic Semiconductor ASA
- *
- * All rights reserved.
- *
- * Redistribution and use in source and binary forms, with or without modification,
- * are permitted provided that the following conditions are met:
- *
- * 1. Redistributions of source code must retain the above copyright notice, this
- *    list of conditions and the following disclaimer.
- *
- * 2. Redistributions in binary form, except as embedded into a Nordic
- *    Semiconductor ASA integrated circuit in a product or a software update for
- *    such product, must reproduce the above copyright notice, this list of
- *    conditions and the following disclaimer in the documentation and/or other
- *    materials provided with the distribution.
- *
- * 3. Neither the name of Nordic Semiconductor ASA nor the names of its
- *    contributors may be used to endorse or promote products derived from this
- *    software without specific prior written permission.
- *
- * 4. This software, with or without modification, must only be used with a
- *    Nordic Semiconductor ASA integrated circuit.
- *
- * 5. Any software provided in binary form under this license must not be reverse
- *    engineered, decompiled, modified and/or disassembled.
- *
- * THIS SOFTWARE IS PROVIDED BY NORDIC SEMICONDUCTOR ASA "AS IS" AND ANY EXPRESS
- * OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES
- * OF MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
- * DISCLAIMED. IN NO EVENT SHALL NORDIC SEMICONDUCTOR ASA OR CONTRIBUTORS BE
- * LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
- * CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE
- * GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
- * HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT
- * LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
- * OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
- *
- */
-/** @file
- *
- * @defgroup ble_sdk_app_beacon_main main.c
- * @{
- * @ingroup ble_sdk_app_beacon
- * @brief Beacon Transmitter Sample Application main file.
- *
- * This file contains the source code for an Beacon transmitter sample application.
- */
-
 #include <stdbool.h>
 #include <stdint.h>
 #include "nordic_common.h"
@@ -67,7 +18,7 @@
 
 #define APP_BLE_CONN_CFG_TAG            1                                  /**< A tag identifying the SoftDevice BLE configuration. */
 
-#define NON_CONNECTABLE_ADV_INTERVAL    MSEC_TO_UNITS(100, UNIT_0_625_MS)  /**< The advertising interval for non-connectable advertisement (100 ms). This value can vary between 100ms to 10.24s). */
+#define NON_CONNECTABLE_ADV_INTERVAL    BLE_GAP_ADV_INTERVAL_MAX
 
 #define APP_BEACON_INFO_LENGTH          0x17                               /**< Total length of information advertised by the Beacon. */
 #define APP_ADV_DATA_LENGTH             0x15                               /**< Length of manufacturer specific data in the advertisement. */
@@ -98,19 +49,14 @@ static ble_gap_adv_params_t m_adv_params;                                  /**< 
 static uint8_t              m_adv_handle = BLE_GAP_ADV_SET_HANDLE_NOT_SET; /**< Advertising handle used to identify an advertising set. */
 static uint8_t              m_enc_advdata[BLE_GAP_ADV_SET_DATA_SIZE_MAX];  /**< Buffer for storing an encoded advertising set. */
 
-/**@brief Struct that contains pointers to the encoded advertising data. */
-static ble_gap_adv_data_t m_adv_data =
-{
-    .adv_data =
-    {
+static ble_gap_adv_data_t m_adv_data = {
+    .adv_data = {
         .p_data = m_enc_advdata,
         .len    = BLE_GAP_ADV_SET_DATA_SIZE_MAX
     },
-    .scan_rsp_data =
-    {
+    .scan_rsp_data = {
         .p_data = NULL,
         .len    = 0
-
     }
 };
 
@@ -128,31 +74,11 @@ static uint8_t m_beacon_info[APP_BEACON_INFO_LENGTH] =                    /**< I
                          // this implementation.
 };
 
-
-/**@brief Callback function for asserts in the SoftDevice.
- *
- * @details This function will be called in case of an assert in the SoftDevice.
- *
- * @warning This handler is an example only and does not fit a final product. You need to analyze
- *          how your product is supposed to react in case of Assert.
- * @warning On assert from the SoftDevice, the system can only recover on reset.
- *
- * @param[in]   line_num   Line number of the failing ASSERT call.
- * @param[in]   file_name  File name of the failing ASSERT call.
- */
-void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name)
-{
+void assert_nrf_callback(uint16_t line_num, const uint8_t * p_file_name) {
     app_error_handler(DEAD_BEEF, line_num, p_file_name);
 }
 
-/**@brief Function for initializing the Advertising functionality.
- *
- * @details Encodes the required advertising data and passes it to the stack.
- *          Also builds a structure to be passed to the stack when starting advertising.
- */
-static void advertising_init(void)
-{
-    uint32_t      err_code;
+static void advertising_init(void) {
     ble_advdata_t advdata;
     uint8_t       flags = BLE_GAP_ADV_FLAG_BR_EDR_NOT_SUPPORTED;
 
@@ -160,106 +86,35 @@ static void advertising_init(void)
 
     manuf_specific_data.company_identifier = APP_COMPANY_IDENTIFIER;
 
-#if defined(USE_UICR_FOR_MAJ_MIN_VALUES)
-    // If USE_UICR_FOR_MAJ_MIN_VALUES is defined, the major and minor values will be read from the
-    // UICR instead of using the default values. The major and minor values obtained from the UICR
-    // are encoded into advertising data in big endian order (MSB First).
-    // To set the UICR used by this example to a desired value, write to the address 0x10001080
-    // using the nrfjprog tool. The command to be used is as follows.
-    // nrfjprog --snr <Segger-chip-Serial-Number> --memwr 0x10001080 --val <your major/minor value>
-    // For example, for a major value and minor value of 0xabcd and 0x0102 respectively, the
-    // the following command should be used.
-    // nrfjprog --snr <Segger-chip-Serial-Number> --memwr 0x10001080 --val 0xabcd0102
-    uint16_t major_value = ((*(uint32_t *)UICR_ADDRESS) & 0xFFFF0000) >> 16;
-    uint16_t minor_value = ((*(uint32_t *)UICR_ADDRESS) & 0x0000FFFF);
-
-    uint8_t index = MAJ_VAL_OFFSET_IN_BEACON_INFO;
-
-    m_beacon_info[index++] = MSB_16(major_value);
-    m_beacon_info[index++] = LSB_16(major_value);
-
-    m_beacon_info[index++] = MSB_16(minor_value);
-    m_beacon_info[index++] = LSB_16(minor_value);
-#endif
-
     manuf_specific_data.data.p_data = (uint8_t *) m_beacon_info;
     manuf_specific_data.data.size   = APP_BEACON_INFO_LENGTH;
 
-    // Build and set advertising data.
     memset(&advdata, 0, sizeof(advdata));
-
     advdata.name_type             = BLE_ADVDATA_NO_NAME;
     advdata.flags                 = flags;
     advdata.p_manuf_specific_data = &manuf_specific_data;
 
-    // Initialize advertising parameters (used when starting advertising).
     memset(&m_adv_params, 0, sizeof(m_adv_params));
-
     m_adv_params.properties.type = BLE_GAP_ADV_TYPE_NONCONNECTABLE_NONSCANNABLE_UNDIRECTED;
     m_adv_params.p_peer_addr     = NULL;    // Undirected advertisement.
     m_adv_params.filter_policy   = BLE_GAP_ADV_FP_ANY;
     m_adv_params.interval        = NON_CONNECTABLE_ADV_INTERVAL;
-    m_adv_params.duration        = 0;       // Never time out.
+    m_adv_params.duration        = 1; // only send one advertisement
 
-    err_code = ble_advdata_encode(&advdata, m_adv_data.adv_data.p_data, &m_adv_data.adv_data.len);
-    APP_ERROR_CHECK(err_code);
-
-    err_code = sd_ble_gap_adv_set_configure(&m_adv_handle, &m_adv_data, &m_adv_params);
-    APP_ERROR_CHECK(err_code);
+    APP_ERROR_CHECK(ble_advdata_encode(&advdata, m_adv_data.adv_data.p_data, &m_adv_data.adv_data.len));
+    APP_ERROR_CHECK(sd_ble_gap_adv_set_configure(&m_adv_handle, &m_adv_data, &m_adv_params));
 }
 
-
-/**@brief Function for starting advertising.
- */
-static void advertising_start(void)
-{
-    ret_code_t err_code;
-
-    err_code = sd_ble_gap_adv_start(m_adv_handle, APP_BLE_CONN_CFG_TAG);
-    APP_ERROR_CHECK(err_code);
-
-    err_code = bsp_indication_set(BSP_INDICATE_ADVERTISING);
-    APP_ERROR_CHECK(err_code);
+static void advertising_start(void) {
+    APP_ERROR_CHECK(sd_ble_gap_adv_start(m_adv_handle, APP_BLE_CONN_CFG_TAG));
 }
 
+static void ble_stack_init(void) {
+    APP_ERROR_CHECK(nrf_sdh_enable_request());
 
-/**@brief Function for initializing the BLE stack.
- *
- * @details Initializes the SoftDevice and the BLE event interrupt.
- */
-static void ble_stack_init(void)
-{
-    ret_code_t err_code;
-
-    err_code = nrf_sdh_enable_request();
-    APP_ERROR_CHECK(err_code);
-
-    // Configure the BLE stack using the default settings.
-    // Fetch the start address of the application RAM.
     uint32_t ram_start = 0;
-    err_code = nrf_sdh_ble_default_cfg_set(APP_BLE_CONN_CFG_TAG, &ram_start);
-    APP_ERROR_CHECK(err_code);
-
-    // Enable BLE stack.
-    err_code = nrf_sdh_ble_enable(&ram_start);
-    APP_ERROR_CHECK(err_code);
-}
-
-
-/**@brief Function for initializing logging. */
-static void log_init(void)
-{
-    ret_code_t err_code = NRF_LOG_INIT(NULL);
-    APP_ERROR_CHECK(err_code);
-
-    NRF_LOG_DEFAULT_BACKENDS_INIT();
-}
-
-/**@brief Function for initializing LEDs. */
-static void leds_init(void)
-{
-    ret_code_t err_code = bsp_init(BSP_INIT_LEDS, NULL);
-    APP_ERROR_CHECK(err_code);
+    APP_ERROR_CHECK(nrf_sdh_ble_default_cfg_set(APP_BLE_CONN_CFG_TAG, &ram_start));
+    APP_ERROR_CHECK(nrf_sdh_ble_enable(&ram_start));
 }
 
 static volatile bool m_xfer_done = false;
@@ -268,21 +123,16 @@ static struct {
     uint16_t humidity;
 } m_sample;
 
-static void read_event(void * p_context)
-{
-    nrf_gpio_pin_toggle(5);
-    ret_code_t err_code;
-
+static void read_event(void * p_context) {
+    //nrf_gpio_pin_toggle(5);
     uint8_t reg = 0x0;
 
     m_xfer_done = false;
-    err_code = nrf_drv_twi_tx(&m_twi, SENSOR_ADDR, &reg, 1, true);
-    APP_ERROR_CHECK(err_code);
+    APP_ERROR_CHECK(nrf_drv_twi_tx(&m_twi, SENSOR_ADDR, &reg, 1, true));
     while (m_xfer_done == false);
 
     m_xfer_done = false;
-    err_code = nrf_drv_twi_rx(&m_twi, SENSOR_ADDR, (uint8_t*) &m_sample, sizeof(m_sample));
-    APP_ERROR_CHECK(err_code);
+    APP_ERROR_CHECK(nrf_drv_twi_rx(&m_twi, SENSOR_ADDR, (uint8_t*) &m_sample, sizeof(m_sample)));
     while (m_xfer_done == false);
 
     int16_t temp = m_sample.temp * 165 * 100 / UINT16_MAX - 4000;
@@ -294,55 +144,24 @@ static void read_event(void * p_context)
     m_beacon_info[20] = humidity >> 8;
     m_beacon_info[21] = humidity & 0xff;
 
-    sd_ble_gap_adv_stop(m_adv_handle);
     advertising_init();
     advertising_start();
 }
 
-
-static void start_event(void * p_context)
-{
-    nrf_gpio_pin_toggle(5);
+static void start_event(void * p_context) {
+    //nrf_gpio_pin_toggle(5);
     uint8_t regs[] = {0x0f, 0x01};
 
     m_xfer_done = false;
-    uint32_t err_code = nrf_drv_twi_tx(&m_twi, SENSOR_ADDR, regs, 2, false);
-    APP_ERROR_CHECK(err_code);
+    APP_ERROR_CHECK(nrf_drv_twi_tx(&m_twi, SENSOR_ADDR, regs, 2, false));
     while (m_xfer_done == false);
 
     // give sensor time to measure (~700us needed)
     app_timer_start(m_timer_read_id, APP_TIMER_TICKS(1), NULL);
 }
 
-/**@brief Function for initializing timers. */
-static void timers_init(void)
-{
-    ret_code_t err_code = app_timer_init();
-    APP_ERROR_CHECK(err_code);
-
-    err_code = app_timer_create(&m_timer_id,
-                                APP_TIMER_MODE_REPEATED,
-                                start_event);
-    APP_ERROR_CHECK(err_code);
-    err_code = app_timer_create(&m_timer_read_id,
-                                APP_TIMER_MODE_SINGLE_SHOT,
-                                read_event);
-    APP_ERROR_CHECK(err_code);
-}
-
-
-/**@brief Function for initializing power management.
- */
-static void power_management_init(void)
-{
-    ret_code_t err_code = nrf_pwr_mgmt_init();
-    APP_ERROR_CHECK(err_code);
-}
-
-void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
-{
-    switch (p_event->type)
-    {
+void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context) {
+    switch (p_event->type) {
         case NRF_DRV_TWI_EVT_DONE:
             m_xfer_done = true;
             break;
@@ -351,8 +170,7 @@ void twi_handler(nrf_drv_twi_evt_t const * p_event, void * p_context)
     }
 }
 
-void twi_init (void)
-{
+void twi_init (void) {
     const nrf_drv_twi_config_t twi_config = {
        .scl                = 9,
        .sda                = 6,
@@ -361,62 +179,41 @@ void twi_init (void)
        .clear_bus_init     = false
     };
 
-    ret_code_t err_code = nrf_drv_twi_init(&m_twi, &twi_config, twi_handler, NULL);
-    APP_ERROR_CHECK(err_code);
+    APP_ERROR_CHECK(nrf_drv_twi_init(&m_twi, &twi_config, twi_handler, NULL));
 
     nrf_drv_twi_enable(&m_twi);
 }
 
-/**@brief Function for handling the idle state (main loop).
- *
- * @details If there is no pending log operation, then sleep until next the next event occurs.
- */
-static void idle_state_handle(void)
-{
-    if (NRF_LOG_PROCESS() == false)
-    {
-        nrf_pwr_mgmt_run();
-    }
-}
-
-
-/**
- * @brief Function for application main entry.
- */
-int main(void)
-{
+int main(void) {
     nrf_gpio_cfg_output(5);
-    // Initialize.
-    log_init();
-    timers_init();
-    leds_init();
+
+    APP_ERROR_CHECK(NRF_LOG_INIT(NULL));
+    NRF_LOG_DEFAULT_BACKENDS_INIT();
+
+    APP_ERROR_CHECK(app_timer_init());
+    APP_ERROR_CHECK(app_timer_create(&m_timer_id, APP_TIMER_MODE_REPEATED, start_event));
+    APP_ERROR_CHECK(app_timer_create(&m_timer_read_id, APP_TIMER_MODE_SINGLE_SHOT, read_event));
+
+    APP_ERROR_CHECK(bsp_init(BSP_INIT_LEDS, NULL));
     twi_init();
-    power_management_init();
+    APP_ERROR_CHECK(nrf_pwr_mgmt_init());
+
     ble_stack_init();
     advertising_init();
 
-    // Start execution.
     NRF_LOG_INFO("Beacon example started.");
     advertising_start();
 
-
     // reset
     uint8_t regs[] = {0x0e, 0x80};
-
     m_xfer_done = false;
-    ret_code_t err_code = nrf_drv_twi_tx(&m_twi, SENSOR_ADDR, regs, 2, false);
-    APP_ERROR_CHECK(err_code);
+    APP_ERROR_CHECK(nrf_drv_twi_tx(&m_twi, SENSOR_ADDR, regs, 2, false));
     while (m_xfer_done == false);
 
-    app_timer_start(m_timer_id, APP_TIMER_TICKS(1000), NULL);
+    app_timer_start(m_timer_id, APP_TIMER_TICKS(5000), NULL);
 
-    for (;; )
-    {
-        idle_state_handle();
+    for (;; ) {
+        if (NRF_LOG_PROCESS() == false)
+            nrf_pwr_mgmt_run();
     }
 }
-
-
-/**
- * @}
- */
